02fe270d357f2fa5282ea790be03282dfbcad3d2
Filip Bergman <fi5731be-s@student.lu.se>
2020-09-17 22:58:35 +0200

    Enemy AI almost done

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public static GameMaster gm;
+
+    private void Start() {
+        if (gm == null) {
+            gm = this;
+        }
+    }
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public float spawnDelay = 2f;
+    public Transform spawnPrefab;
+    
+    public IEnumerator RespawnPlayer()
+    {
+        GetComponent<AudioSource>().Play();
+        
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as Transform;
+        Destroy(clone.gameObject, 3f);
+        
+    } 
+    
+    public static void KillPlayer(Player player) {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+    
+    
+    
+}
